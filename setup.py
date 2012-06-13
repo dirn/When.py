479f3ca83afd6a68b9f1980e4ed856ff441b39f5
Andy Dirnberger <dirn@dirnonline.com>
2012-06-12 23:12:42 -0400

    Update 0.1.x to 0.1.1
    This should address the issues with pip install.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if sys.argv[-1] == 'publish':
 
 settings.update(
     name='whenpy',
-    version='0.1.0',
+    version='0.1.1',
     description='Friendly Dates and Times',
     long_description=open('README.rst').read(),
     author='Andy Dirnberger',
