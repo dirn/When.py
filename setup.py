7ef952010f1bbfb9f78de923caa1112121328324
Andy Dirnberger <dirn@dirnonline.com>
2012-06-12 22:54:48 -0400

    Add new Python versions to the supported languages
    Now that tests are passing for Python 3.1 and 3.2, they should go into the list of supported versions of Python listed on PyPI.

--- setup.py
+++ setup.py
@@ -38,6 +38,8 @@ settings.update(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ),
 )
