2e20bb2313f06dc6bf4fb59e15bea38a3985269d
Andy Dirnberger <dirn@dirnonline.com>
2013-01-22 00:16:41 -0500

    Add Python 3.3, drop Python 3.1
    The test configurations now include Python 3.3. They no longer include Python 3.1.

--- setup.py
+++ setup.py
@@ -39,8 +39,10 @@ settings.update(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ),
 )
